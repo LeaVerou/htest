f4951d87dc58486f1af068b25417e46625efab05
Lea Verou <lea@verou.me>
2023-11-01 02:06:39 -0400

    First stab at a render() function to render JS-first tests to HTML

---
+++ src/js/render.js
@@ -0,0 +1,83 @@
+/**
+ * Render JS-first tests to HTML
+ */
+
+import Test from "./classes/Test.js";
+import TestResult from "./classes/TestResult.js";
+import RefTest from "../html/reftest.js";
+import { create, output } from "../html/util.js";
+import { formatDuration } from "../util.js";
+
+export default function render (test) {
+	let root = new Test(test);
+
+	create("h1", {
+		textContent: root.name,
+		inside: document.body,
+	});
+
+	let testRows = new Map();
+
+	root.tests?.map?.(t => {
+		let tests = [];
+
+		if (t.isTest) {
+			tests.push(t);
+		}
+
+		if (t.isGroup) {
+			tests.push(...t.tests);
+		}
+
+		let table;
+		let section = create("section", {
+			contents: [
+				{tag: "h1", textContent: t.name},
+				table = create({tag: "table", class: "manual reftest",
+					contents: tests.map((t2, i) => {
+						let tr = t2.render?.() ?? create("tr", {
+							title: t2.name,
+							contents: [
+								{tag: "td", textContent: t2.args?.map(a => output(a)).join(", ") },
+								{tag: "td"},
+								{tag: "td", textContent: output(t2.expect) }
+							]
+						});
+
+						if (t2.throws) {
+							tr.dataset.error = "";
+						}
+
+						testRows.set(t2, tr);
+						return tr;
+					})
+				})
+			],
+			inside: document.body,
+		});
+
+		requestAnimationFrame(() => {
+			if (!table.reftest) {
+				new RefTest(table);
+			}
+		})
+
+		return section;
+	});
+
+	let result = new TestResult(root);
+
+	result.addEventListener("done", e => {
+		let target = e.detail.target;
+
+		if (target.test.isTest) {
+			let tr = testRows.get(target.test);
+			tr.cells[1].textContent = output(target.actual);
+			tr.classList.add(target.pass ? "pass" : "fail");
+			tr.dataset.time = formatDuration(target.timeTaken);
+		}
+	});
+
+	result.runAll();
+}
+
