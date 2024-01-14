07c30f6a26683e67abacb1755e2a9355932a0cb1
Dmitry Sharabin <dmitrysharabin@gmail.com>
2024-01-14 20:27:35 +0100

    Fix output

--- src/js/classes/TestResult.js
+++ src/js/classes/TestResult.js
@@ -218,7 +218,7 @@ ${ this.error.stack }`);
 					}
 				}
 				else {
-					message += "which doesn’t pass the test provided";
+					message += " which doesn’t pass the test provided";
 				}
 
 				ret.details.push(message);
