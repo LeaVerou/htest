3f4a9b7916c02dad20c769e47884df54b2ae0b08
Lea Verou <lea@verou.me>
2024-03-13 23:59:07 -0400

    Add src/index.js

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import * as map from "./map.js";
+import * as check from "/check.js";
+import render from "./js/render.js";
+import run from "./js/node-run.js";
+
+export {
+	map,
+	check,
+	render,
+	run,
+}
