951966e9abe29c821bb3177ac2a15d47408d143f
Lea Verou <lea@verou.me>
2024-03-16 20:31:00 -0400

    Print console tree

--- src/env/console.js
+++ src/env/console.js
@@ -1,5 +1,18 @@
 import format from "../format-console.js";
 
+function printTree (str, parent) {
+	if (str.children?.length > 0) {
+		console["group" + (parent? "Collapsed" : "")](format(str));
+		for (let child of str.children) {
+			printTree(child, str);
+		}
+		console.groupEnd();
+	}
+	else {
+		console.log(format(str));
+	}
+}
+
 /**
  * Environment-agnostic env that prints results in the console (without depending on terminal-specific things)
  */
@@ -8,8 +21,8 @@ export default {
 	resolveLocation (test) {
 		return import(test).then(m => m.default ?? m);
 	},
-	done (result, options, event) {
+	finish (result, options, event) {
 		let str = result.toString({ format: options.format ?? "rich" });
-		console.log(format(str));
+		printTree(str);
 	}
 }
