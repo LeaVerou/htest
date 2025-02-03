5759857b1b818adcb5659c566110ddc3b1c209b7
Dmitry Sharabin <dmitrysharabin@gmail.com>
2025-02-03 16:14:08 +0100

    [HTML mode] Make test isolation and the navigation between individual tests work again (#72)
    * Add some margin to make the scrolled-into-view test fully visible
    
    * Consider as a test not only direct children of `tbody` but also direct children of the reftest table itself (the exact case when we render JS-first tests)
    
    * Generate unique ids (sometimes, tests might have the same name, so we can't simply use them as ids). Without unique ids, the isolation feature doesn't work as expected

--- src/html/testpage.js
+++ src/html/testpage.js
@@ -59,9 +59,10 @@ loadCSS("https://unpkg.com/tippy.js@1.3.0/dist/tippy.css");
 await ready();
 
 // Add ids to all tests
-$$(`.reftest > tbody > tr`).forEach((test, i) => {
+$$(`.reftest > tbody > tr, .reftest > tr`).forEach((test, i) => {
 	if (!test.id) {
-		test.id = idify(test.title) || "test-" + (i + 1);
+		let id = idify(test.title);
+		test.id = id + (id ? "-" : "") + "test-" + (i + 1);
 	}
 });
 
@@ -75,7 +76,7 @@ let hashchanged = evt => {
 		}
 
 		let isSection = target.matches("body > section");
-		let isTest = target.matches(".reftest > tbody > tr");
+		let isTest = target.matches(".reftest > tbody > tr, .reftest > tr");
 
 		if (isSection || isTest) {
 			if (evt) {
@@ -95,7 +96,7 @@ let hashchanged = evt => {
 			}
 
 			if (isTest) {
-				for (let test of $$(`.reftest > tbody > tr`)) {
+				for (let test of $$(`.reftest > tbody > tr, .reftest > tr`)) {
 					if (test.id !== target.id) {
 						test.remove();
 					}
@@ -126,7 +127,7 @@ hashchanged();
 window.addEventListener("hashchange", hashchanged);
 
 // Add div for counter at the end of body
-let nav = create({
+RefTest.nav = create({
 	tag: "nav",
 	inside: document.body,
 	contents: [
@@ -193,7 +194,7 @@ function loadCSS (url) {
 // Isolate specific test
 document.addEventListener("dblclick", evt => {
 	if (evt.altKey) {
-		let test = evt.target.closest(".reftest > tbody > tr");
+		let test = evt.target.closest(".reftest > tbody > tr, .reftest > tr");
 
 		if (test) {
 			location.hash = test.id;
